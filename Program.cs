2e86ea40864edb30e859309354ce1967aff9716e
samb <sam.baker@calastone.com>
2023-12-04 10:40:37 +0000

    Tidy up

--- Program.cs
+++ Program.cs
@@ -1,26 +1,24 @@
-﻿using System.Numerics;
-
-Console.WriteLine(
+﻿Console.WriteLine(
     DateTime.Now.Day switch
     {
         1 => File.ReadAllLines("Day1.txt")
-        .Select(l => new string(l
-            .Select((c, i) => char.IsDigit(c)
-                ? c
-                : (new string(l.Skip(i).Take(5).ToArray()) switch
-                {
-                    var s when s.StartsWith("one") => '1',
-                    var s when s.StartsWith("two") => '2',
-                    var s when s.StartsWith("three") => '3',
-                    var s when s.StartsWith("four") => '4',
-                    var s when s.StartsWith("five") => '5',
-                    var s when s.StartsWith("six") => '6',
-                    var s when s.StartsWith("seven") => '7',
-                    var s when s.StartsWith("eight") => '8',
-                    var s when s.StartsWith("nine") => '9',
-                    _ => 'x',
-                })).ToArray()))
-        .Select(l => int.Parse(l.First(char.IsDigit).ToString()) * 10 + int.Parse(l.Last(char.IsDigit).ToString())).Sum(),
+            .Select(l => new string(l
+                .Select((c, i) => char.IsDigit(c)
+                    ? c
+                    : (new string(l.Skip(i).Take(5).ToArray()) switch
+                    {
+                        var s when s.StartsWith("one") => '1',
+                        var s when s.StartsWith("two") => '2',
+                        var s when s.StartsWith("three") => '3',
+                        var s when s.StartsWith("four") => '4',
+                        var s when s.StartsWith("five") => '5',
+                        var s when s.StartsWith("six") => '6',
+                        var s when s.StartsWith("seven") => '7',
+                        var s when s.StartsWith("eight") => '8',
+                        var s when s.StartsWith("nine") => '9',
+                        _ => 'x',
+                    })).ToArray()))
+            .Select(l => int.Parse(l.First(char.IsDigit).ToString()) * 10 + int.Parse(l.Last(char.IsDigit).ToString())).Sum(),
         2 => File.ReadAllLines("Day2.txt")
             .Select(l => new string(l.SkipWhile(c => c != ':').Skip(1).ToArray()))
             .Select(l => l.Split(";"))
